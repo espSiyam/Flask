d2e4371374d9b67c498a8e19c396feac85c99add
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2020-10-08 01:46:25 +0600

    Update flaskblog.py

--- flaskblog.py
+++ flaskblog.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 def hello():
 	return "<h1>Home Page!</h1>"
 
+@app.route("/About")
+def about():
+	return "<h1>About Page!</h1>"
+
+@app.route("/Home")
+def ahello():
+	return "<h1>Another Home Page!</h1>"
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
