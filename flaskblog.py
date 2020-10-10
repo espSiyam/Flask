9fa59f6858e522449a6a25ffd5a49b3a84d45c5b
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2020-10-10 19:27:22 +0600

    4th

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,29 @@
-from flask import Flask 
+from flask import Flask , render_template, url_for
 app = Flask(__name__)
 
+posts = [
+	{
+		'author': 'Sohag Ahammed Siyam',
+		'title': 'Blog post 1',
+		'content': 'First blog content',
+		'date_posted': 'April 20, 2018'
+	},
+	{
+		'author': 'Anis ul Haque',
+		'title': 'Blog post 2',
+		'content': 'Second blog content',
+		'date_posted': 'April 21, 2018'		
+	}
+]
+
 @app.route("/")
+@app.route("/home")
 def hello():
-	return "<h1>Home Page!</h1>"
+	return render_template('home.html', posts = posts, title='Home')
 
-@app.route("/About")
+@app.route("/about")
 def about():
-	return "<h1>About Page!</h1>"
-
-@app.route("/Home")
-def ahello():
-	return "<h1>Another Home Page!</h1>"
+	return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
