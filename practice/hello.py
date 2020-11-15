1918e7ff82ec381d9ac896df3b2df118fa263da5
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2020-11-15 16:25:41 +0600

    ab

--- practice/hello.py
+++ practice/hello.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!\nI am Siyam'
 
+@app.route('/about')
+def about():
+    return 'This is about PAGE'
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
