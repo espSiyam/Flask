abad4013a9beaefdc555e06edb470dd6719f1356
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2020-11-15 17:26:53 +0600

    post

--- practice/hello.py
+++ practice/hello.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+	{
+		'author': 'Sohag Ahammed Siyam',
+		'title': 'Blog post 1',
+		'content': 'First blog content',
+		'date_posted': 'April 20, 2018'
+	},
+	{
+		'author': 'Anis ul Haque',
+		'title': 'Blog post 2',
+		'content': 'Second blog content',
+		'date_posted': 'April 21, 2018'		
+	}
+]
+
 @app.route('/')
 def hello_world():
     return render_template('home.html')
