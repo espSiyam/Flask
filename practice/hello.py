45670b96550b2ac918af553fbcc89ff59bc5d368
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2020-11-18 14:01:36 +0600

    a

--- practice/hello.py
+++ practice/hello.py
@@ -42,7 +42,7 @@ def register():
 @app.route('/login')
 def login():
 	form = LoginForm()
-	return render_template('login.html', title='Login', form = form )
+	return render_template('login.html', title='Login',form=form)
 
 
 if __name__ == '__main__':
