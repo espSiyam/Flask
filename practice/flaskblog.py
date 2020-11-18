2a8a7be71787128dd1ad525e23e62d234efbc526
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2020-11-19 01:30:59 +0600

    imports

--- practice/flaskblog.py
+++ practice/flaskblog.py
@@ -1,13 +1,3 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-from flask_sqlalchemy import SQLAlchemy
-
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '0659b56c3a4fb43903d3a5035f55b7e2'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
 from models import User, Post
 posts = [
 	{ 
