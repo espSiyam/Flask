fb715bff3a89acc868ca4eec470ad7b96c11b94a
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2020-11-19 22:34:02 +0600

    email

--- practice/flaskblog/forms.py
+++ practice/flaskblog/forms.py
@@ -14,10 +14,16 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    def validate_username(self, username)
-    user = User.query.filter_by(username=username.data).first()
-    if user:
-        raise ValidationError('That username is taken. Please choose another one.')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()   
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()   
+        if email:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
