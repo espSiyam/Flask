fc43c70e0816edc118ecb15e1e7121412d410745
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2020-11-20 02:09:32 +0600

    private link

--- practice/flaskblog/routes.py
+++ practice/flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, flash, redirect
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 from flaskblog import app, db, bcrypt
-from flask_login import login_user, current_user, logout_user 
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
 	{ 
@@ -64,3 +64,9 @@ def login():
 def logout():
 	logout_user()
 	return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+	return render_template('account.html', title='Account')
+
