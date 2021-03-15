2c652c491397f34c588ff85717d65f786c1ced0f
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2021-03-16 02:12:01 +0600

    next

--- 1.Bootcamp/4.flaskBasic/basic.py
+++ 1.Bootcamp/4.flaskBasic/basic.py
@@ -4,10 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-	name = "Siyam"
-	letters = list(name)
-	pupp = {'pup_name':'doggo'}
-	return render_template('basic.html', var1 = name, var2 = letters, var3 = pupp)
+	mylist = [9,2,3,4,5]
+	return render_template('basic.html', list = mylist)
 
 if __name__ == "__main__":
 	app.run(debug=True)
