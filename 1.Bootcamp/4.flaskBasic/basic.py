3a09fe64b26840e1f234011f0eccbd06696ee218
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2021-03-16 01:44:29 +0600

    variable

--- 1.Bootcamp/4.flaskBasic/basic.py
+++ 1.Bootcamp/4.flaskBasic/basic.py
@@ -3,15 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('basic.html')
 
-@app.route('/info')
-def info():
-	return '<h1>Puppes are cute!</h1>'
-
-@app.route('/puppy/<name>')
-def puppy(name):
-	return '<h1>100th letter: {}</h1>'.format(name[100])
+	name = "Siyam"
+	return render_template('basic.html', var = name)
 
 if __name__ == "__main__":
 	app.run(debug=True)
