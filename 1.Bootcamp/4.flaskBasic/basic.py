dce4bc4a75be1b73e4b061d3c1471b251ecd9121
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2021-03-16 02:45:15 +0600

    urlfor

--- 1.Bootcamp/4.flaskBasic/basic.py
+++ 1.Bootcamp/4.flaskBasic/basic.py
@@ -3,9 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+	return render_template('home.html')
 
-	mylist = [9,2,3,4,5]
-	return render_template('basic.html', list = mylist)
+@app.route('/puppy/<name>')
+def pup_name(name):
+	return render_template('puppy.html', name = name)
 
 if __name__ == "__main__":
 	app.run(debug=True)
